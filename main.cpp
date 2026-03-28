5d5bda25f77cab854d02c6d312b93833c95da3d1
CadenceRiddle <cadence475@gmail.com>
2026-03-27 20:00:49 -0400

    added argument check

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <fstream>
 
-int main(){
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char* argv[]){
+
+    if (argc != 2) {
+        std::cerr << "Incorrect usage" << std::endl;
+        std::cerr << "hermes <input.hr>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::fstream input(argv[1], std::ios::in);
+
+    std::cout << argv[1] << std::endl;
+    return EXIT_SUCCESS;
 }
