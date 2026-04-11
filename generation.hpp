24f6bb6ccc7ecf8161b8288fba2acecabf6e0557
CadenceRiddle <cadence475@gmail.com>
2026-04-11 16:38:18 -0400

    completed variable stack push feature

--- generation.hpp
+++ generation.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <unordered_map>
 #include "./parser.hpp"
 
 class Generator {
@@ -20,11 +21,20 @@ public:
             };
 
             void operator()(const NodeExprIntLit& expr_int_lit){
-                gen->m_output << "   mov rax, " << expr_int_lit.int_lit.value.value() << "\n";
-                gen->m_output << "   push rax\n";
+                gen->m_output << "    mov rax, " << expr_int_lit.int_lit.value.value() << "\n";
+                gen->push("rax");
             }
             void operator()(const NodeExprIdent& expr_ident){
+                
+                if (gen->m_vars.find(expr_ident.ident.value.value()) == gen->m_vars.end()){
+                    std::cerr << "Undeclared identifier: " << expr_ident.ident.value.value() << std::endl;
+                    exit(EXIT_FAILURE);
+                }
 
+                const auto& var = gen->m_vars.at(expr_ident.ident.value.value());
+                std::stringstream offset;
+                offset << "    QWORD [rsp + " << (gen->m_stack_size - var.stack_loc - 1) * 8 << "]\n";
+                gen->push(offset.str());
             }
         };
 
@@ -35,16 +45,28 @@ public:
     void gen_stmt(const NodeStmt& stmt) {
         struct StmtVisitor {
             Generator* gen;
+
+            StmtVisitor(Generator* gen)
+                : gen(gen)
+            {
+            };
+
             void operator()(const NodeStmtExit& stmt_exit){
                 gen->gen_expr(stmt_exit.expr);
                 gen->m_output << "    mov rax, 60\n";
-                gen->m_output << "    pop rdi\n";
-                gen->m_output << "    mov rdi, 0\n"; //TODO
+                gen->pop("rdi");
                 gen->m_output << "    syscall\n";
             }
             
             void operator()(const NodeStmtLet& stmt_let){
+                if(gen->m_vars.find(stmt_let.ident.value.value()) != gen->m_vars.end()){
+                    std::cerr << "Identifier already used: " << stmt_let.ident.value.value();
+                    exit(EXIT_FAILURE);
+                }
 
+                
+                gen->m_vars.insert({stmt_let.ident.value.value(), Var {gen->m_stack_size}});
+                gen->gen_expr(stmt_let.expr);
             }
         };
 
@@ -67,6 +89,23 @@ public:
     }
 
 private:
+
+    void push(const std::string& reg){
+        m_output << "    push " << reg << "\n";
+        m_stack_size++;
+    }
+
+    void pop(const std::string& reg){
+        m_output << "    pop " << reg << "\n";
+        m_stack_size--;
+    }
+
+    struct Var {
+        size_t stack_loc;
+    };
+
     const NodeProgram m_prog;
     std::stringstream m_output;
+    size_t m_stack_size = 0;
+    std::unordered_map<std::string, Var> m_vars {};
 };
